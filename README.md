b58af407941da710ba64722c11b25b756aa8a825
Karl Guzman <guzmankarl0410@gmail.com>
2021-01-26 16:56:42 -0800

    installed dependencies

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 //following this project from a Youtube course
 //source : https://www.youtube.com/watch?v=khJlrj3Y6Ls
 
+//dependencies installed
+//axios, react-chartjs-2 react-countup
+
 //My own implemented changes below:
---
+++ src/App.js
@@ -0,0 +1,17 @@
+//class based component from the video
+
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <main>
+                    <h1>Covid Tracker App</h1>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default App
