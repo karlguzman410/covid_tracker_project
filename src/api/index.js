825ab85a0acbf98a522b0d5eda5d96aa41763b6b
Karl Guzman <guzmankarl0410@gmail.com>
2021-01-26 17:43:44 -0800

    filtered data from API to only return data needed

--- src/api/index.js
+++ src/api/index.js
@@ -6,9 +6,9 @@ const url = 'https://covid19.mathdro.id/api'
 //asynchronous function
 export const fetchData = async () => {
     try {
-        const response = await axios.get(url)
-        console.log(response)
-        return response
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url)
+        //return only the data we are needing
+        return { confirmed, recovered, deaths, lastUpdate }
     } catch (error) {
 
     }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-function Cards() {
+function Cards(props) {
+  console.log(props);
   return (
     <div>
       <h1>Cards</h1>
