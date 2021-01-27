eefa2671502b3e8ec43f5ca0a2fce63c8c83d676
Karl Guzman <guzmankarl0410@gmail.com>
2021-01-26 17:23:32 -0800

    added components, project structure, and async func to call API and get data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 //class based component from the video
 
 import React, { Component } from 'react'
+import { Cards, Chart, CountryPicker } from './components'
+import { fetchData } from './api'
+import styles from './App.module.css'
 
 class App extends Component {
+
+    async componentDidMount() {
+        const data = await fetchData()
+        console.log(data)
+    }
+
     render() {
         return (
-            <div>
-                <main>
-                    <h1>Covid Tracker App</h1>
-                </main>
+            <div className={styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
